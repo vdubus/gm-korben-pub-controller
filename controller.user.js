21ec70e610b27321fa7748e019b1c62783646349
Vianney Dubus <vdubus@users.noreply.github.com>
2016-04-24 22:20:33 +0200

    Update name for GM auto install process
    Update script name to match the GM recommended name.
    Update also the installation URL.

--- controller.user.js
+++ controller.user.js
@@ -5,8 +5,8 @@
 // @include     http://korben.info/*
 // @version     1
 // @grant       none
-// @downloadURL https://cdn.rawgit.com/vdubus/gm-korben-pub-controller/master/controller.js
-// @updateURL   https://cdn.rawgit.com/vdubus/gm-korben-pub-controller/master/controller.js
+// @downloadURL https://cdn.rawgit.com/vdubus/gm-korben-pub-controller/master/controller.user.js
+// @updateURL   https://cdn.rawgit.com/vdubus/gm-korben-pub-controller/master/controller.user.js
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js
 // @require     https://cdn.rawgit.com/jonnyreeves/js-logger/master/src/logger.js
 // ==/UserScript==
